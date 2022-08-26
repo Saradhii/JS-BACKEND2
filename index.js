a27505176eb7f405622a242bdd1f3e93aded157b
Saradhi <saradhi0001069@gmail.com>
2022-08-26 12:24:46 +0530

    basic changes

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const cors = require("cors");
 const TodoRoute = require("./routes/TodoRoute");
 const UserRoute = require("./routes/UserRoute");
-const Flat = require("./models/TodoSchema");
 
+// App creation
 const app = express();
+//Needed middlewares
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(
@@ -13,19 +14,10 @@ app.use(
       origin: ["http://localhost:3000","https://harmonious-clafoutis-0cfde6.netlify.app"],
     })
   );
-  
 app.use("/todo",TodoRoute);
 app.use("/user",UserRoute);
 
-app.get("/",(req,res)=>{
-    res.send("Mock10 working..")
-})
-
-app.get("/allflats",async(req,res)=>{
-    const flats = await Flat.find();
-    res.send(flats);
-})
-
+// starting the server && checking db connection
 const PORT = process.env.PORT || 8060
 app.listen(PORT, async () => {
     try {
